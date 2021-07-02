20077d57545bac35dc56f51a563aa01aae37d9f3
Christian Paranas <christiannparanas@gmail.com>
2021-07-02 22:54:40 +0800

    final

---
+++ src/routes/chapter7.svelte
@@ -0,0 +1,214 @@
+
+<title>Chapter 7</title>
+<main>
+	<div class="chapter chapter 2">
+		<div class="head">
+			<div class="tit">Chapter 7</div>
+
+	
+		<button class="pushable">
+			<a href="/">
+		  <span class="btn">
+		    <i class="far fa-caret-left"></i>
+		  </span>
+		  </a>
+		</button>
+			
+		</div>
+
+		<div class="rr">TV5 Series</div>
+		<div class="dd">
+		<table>
+	<tbody>
+		<tr>
+			<td></td>
+			<td>5am</td>
+			<td>6am</td>
+			<td>6:30am</td>
+			<td>7am</td>
+			<td>7:30am</td>
+			<td>8am</td>
+			<td>8:30am</td>
+		</tr>
+		<tr>
+			<td>Monday</td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/03/Tv-Mass_480x270.jpg" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/05/BATIBOT_480X270.jpg" alt="" /></td>
+			<td>BEN 10</td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/CKND_480x270.png" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/DEXTER_480x270.png" alt="" /></td>
+			<td>GENERATOR REX</td>
+			<td>ADVENTURE TIME</td>
+		</tr>
+		<tr>
+			<td>Tuesday</td>
+
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/03/Tv-Mass_480x270.jpg" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/05/BATIBOT_480X270.jpg" alt="" /></td>
+			<td>BEN 10</td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/CKND_480x270.png" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/DEXTER_480x270.png" alt="" /></td>
+			<td>GENERATOR REX</td>
+			<td>ADVENTURE TIME</td>
+		</tr>
+		<tr>
+			<td>Wednesday</td>
+	
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/03/Tv-Mass_480x270.jpg" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/05/BATIBOT_480X270.jpg" alt="" /></td>
+			<td>BEN 10</td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/CKND_480x270.png" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/DEXTER_480x270.png" alt="" /></td>
+			<td>GENERATOR REX</td>
+			<td>ADVENTURE TIME</td>
+		</tr>
+		<tr>
+			<td>Thursday</td>
+	
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/03/Tv-Mass_480x270.jpg" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/05/BATIBOT_480X270.jpg" alt="" /></td>
+			<td>BEN 10</td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/CKND_480x270.png" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/DEXTER_480x270.png" alt="" /></td>
+			<td>GENERATOR REX</td>
+			<td>ADVENTURE TIME</td>
+		</tr>
+		<tr>
+			<td>Friday</td>
+
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/03/Tv-Mass_480x270.jpg" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/05/BATIBOT_480X270.jpg" alt="" /></td>
+			<td>BEN 10</td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/CKND_480x270.png" alt="" /></td>
+			<td><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2020/07/DEXTER_480x270.png" alt="" /></td>
+			<td>Generator Rex</td>
+			<td>Adventure Time</td>
+		</tr>
+		<tr>
+			<td>Satuday</td>
+			<td></td>
+			<td>Power to Unite</td>
+			<td colspan="3"><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/03/Tv-Mass_480x270.jpg" alt="" /></td>
+			<td>Johnny Bravo</td>
+			<td>Top 20 Funniest</td>
+		</tr>
+		<tr>
+			<td>Sunday</td>
+			<td></td>
+			<td colspan="2"><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/03/Tv-Mass_480x270.jpg" alt="" /></td>
+			<td colspan="2"><img src="http://corpcms.tv5.com.ph/5network/wp-content/uploads/2019/01/featured_Helens-Kitchen.jpg" alt="" /></td>
+			<td colspan="2">NBA</td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+</div>
+	</div>
+</main>
+
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+	main {
+		font-family: 'Montserrat', sans-serif;
+		height: 100%;
+		background: #43294a;
+	}
+
+	.chapter {
+		width: 1200px;
+		margin: 0 auto;
+		padding: 80px 0 100px;
+		color: white;
+	}
+
+	.head {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.tit {
+		font-size: 20px;
+		font-weight: 700;
+		width: fit-content;
+		margin: 0 0 60px;
+		background: #052939;
+		padding: 15px 25px;
+	}
+
+	.pushable {
+		height: fit-content;
+		-webkit-tap-highlight-color: transparent;
+    background: #091a28;
+    border-radius: 50%;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    outline-offset: 4px;
+    outline: none;
+  }
+
+	.btn {
+    display: block;
+    padding: 15px 25px;
+    border-radius: 50%;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.25rem;
+    font-weight: 700;
+    background: #1f2937;
+    color: white;
+    transform: translateY(-6px);
+  }
+
+  .pushable:active .btn {
+    transform: translateY(-2px);
+  }
+
+  .pushable:hover .btn {
+  	background: #fcedd8;
+  	color: #052939;
+  }
+
+	p {
+		margin-top: 0;
+	}
+
+	table {
+		width: 100%;
+	}
+
+	table img {
+		width: 100%;
+		height: 100px;
+	}
+
+	table td {
+		margin: 10px;
+	}
+
+	.dd {
+		overflow-x: auto;
+	}
+
+	.rr {
+		font-size: 25px;
+		font-weight: 800;
+		margin-bottom: 20px;
+	}
+
+	@media only screen and (max-width: 700px) {
+		.chapter {
+			width: 90%;
+			margin: 0 auto;
+			padding-top: 50px;
+			color: white;
+		}
+
+		table {
+			width: 1000px;
+			overflow-x: scroll;
+		}
+	}
+	
+</style>
