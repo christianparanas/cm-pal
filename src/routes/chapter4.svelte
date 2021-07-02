138500ca69cc45188804eea471a1370606323a2b
Christian Paranas <christiannparanas@gmail.com>
2021-07-02 20:29:50 +0800

    chap4

---
+++ src/routes/chapter4.svelte
@@ -0,0 +1,140 @@
+
+<title>Chapter 4</title>
+<main>
+	<div class="chapter chapter 2">
+		<div class="head">
+			<div class="tit">Chapter 4</div>
+
+	
+		<button class="pushable">
+			<a href="/">
+		  <span class="btn">
+		    <i class="far fa-caret-left"></i>
+		  </span>
+		  </a>
+		</button>
+			
+		</div>
+
+
+		<p>
+			<b>Algebraic Equation</b><br>
+			<br><b>Algebraic equation</b>, statement of the equality of two expressions formulated by applying to a set of variables
+			the algebraic operations, namely, addition, subtraction, multiplication, division,raising to a power, and extraction
+			a root. Examples are <i>x<sup>3</sup> + 1</i> and (<i>y<sup>4</sup>x<sup>3</sup> + 2xy - y</i>)/(<i>x-1</i>) = 12. An important special 
+			case of such equations, expresssions of the form <i>axn + bxn<sup>-1</sup> + _ + gx + h=k</i>. They have as many solutions as 
+			their degree(<i>n</i>), and the search for their solutions stimulated much of the development of classical and modern algebra. 
+			Equation like <i>x</i> sin (<i>x</i>) = <i>c</i> that involve nonalgebraic operations, such as logarithms or trigonometric 
+			functions, are said to be transcendental.<br>
+		</p>
+		<p>
+			<br><b>Chemical Compound</b><br>
+			<br>A <b>chemical compound</b>, is a chemical substance consisting of two or more different chemically bonded chemical elements, with a
+			fixed ratio determining the composition.
+			<br>The ratio of each element is usually expressed by chemical formula.
+			<br>For example, water (H<sub>2</sub>O) is a compound consisting of two hydrogen atoms bonded to an oxygen atom.
+			<br>The atoms within a compound can be held together by a variety of interactions, ranging from covalent bonds to 
+			electrostatic forces on ionic bonds.
+			<br>A continuum of bond polarities exist between the purely covalent bond (as in H2) and ionic bonds.
+			<br>For example H<sub>2</sub>O is held together by polar covalent bonds.
+			<br>Sodium chloride is an example of an ionic compound.<br>
+		</p>
+
+		<pre class="prebm">
+			<br>Download Files: <a href=""><b>Algebraic Expression</b></a>
+		<a href=""><b>Chemical Compound</b></a>
+			<br>Source: <a href="https:www.google.com"><b>Google.com</b></a> and <a href="https:www.yahoo.com"><b>Yahoo.com</b></a>
+			<br>Go back to:<br>
+	<a href=""><b>Lesson 1: Algebraic Equation<b></a>
+	<a href=""><b>Lesson 2: Chemical Compound<b></a>
+		</pre>
+	</div>
+</main>
+
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+	main {
+		font-family: 'Montserrat', sans-serif;
+		height: 100%;
+		background: #43294a;
+	}
+
+	.chapter {
+		width: 700px;
+		margin: 0 auto;
+		padding-top: 80px;
+		color: white;
+	}
+
+	.head {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.tit {
+		font-size: 20px;
+		font-weight: 700;
+		width: fit-content;
+		margin: 0 0 60px;
+		background: #052939;
+		padding: 15px 25px;
+	}
+
+	.pushable {
+		height: fit-content;
+		-webkit-tap-highlight-color: transparent;
+    background: #091a28;
+    border-radius: 50%;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    outline-offset: 4px;
+    outline: none;
+  }
+
+	.btn {
+    display: block;
+    padding: 15px 25px;
+    border-radius: 50%;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.25rem;
+    font-weight: 700;
+    background: #1f2937;
+    color: white;
+    transform: translateY(-6px);
+  }
+
+  .pushable:active .btn {
+    transform: translateY(-2px);
+  }
+
+  .pushable:hover .btn {
+  	background: #fcedd8;
+  	color: #052939;
+  }
+
+	p {
+		margin-top: 0;
+		
+	}
+
+	.prebm {
+		padding-bottom: 100px;
+	}
+
+	a {
+		color: pink;
+	}
+
+	@media only screen and (max-width: 700px) {
+		.chapter {
+			width: 90%;
+			margin: 0 auto;
+			padding-top: 50px;
+			color: white;
+		}
+	}
+	
+</style>
